fa38e1bb48a8ea02a0926255fe4523adb534c3aa
Ahmed Hany Farouk <67925988+Ahmed-H300@users.noreply.github.com>
2023-12-02 02:50:06 +0200

    binray file with float for size

--- file.py
+++ file.py
@@ -1,50 +1,47 @@
-import decimal
 import random
 import struct
-
+from typing import Annotated, Dict, List
 import numpy as np
-
 
 class BinaryFile:
     def __init__(self, filename):
         self.filename = filename
         self.vec_size = 70
-        self.length = 24
-        self.id_size = 4
+        self.float_size = 4
+        self.int_size = 4
 
     def insert_row(self, row_id, row_data):
         with open(self.filename, 'ab') as file:
-            # Pack the ID into a binary format
-            packed_id = struct.pack('i', row_id)
-            # Write the packed ID to the file
-            file.write(packed_id)
-            # Convert each numpy.float64 to a fixed-length string and write it to the file
-            for x in row_data:
-                str_x = str(decimal.Decimal(str(x))).ljust(self.length)  # Pad the string with spaces to a length of self.length
-                file.write(str_x.encode())
+            # Pack the ID and the float values into a binary format
+            packed_data = struct.pack(f'i{self.vec_size}f', row_id, *row_data)
+            # Write the packed data to the file
+            file.write(packed_data)
+
+    def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
+        with open(self.filename, 'ab') as file:
+            for row in rows:
+                id, embed = row["id"], row["embed"]
+                # Pack the ID and the float values into a binary format
+                packed_data = struct.pack(f'i{self.vec_size}f', id, *embed)
+                # Write the packed data to the file
+                file.write(packed_data)
+                
 
     def read_row(self, row_id):
         with open(self.filename, 'rb') as file:
             # Calculate the position of the row
-            position = row_id * (self.id_size + self.vec_size * self.length)  # Size of one row (ID + self.vec_size * numpy.float64 values)
+            position = row_id * (self.int_size + self.vec_size * self.float_size)  # Size of one row (ID + vec_size * floats)
             # Seek to the position of the row
             file.seek(position)
-            # Read the ID
-            packed_id = file.read(self.id_size)
-            row_id = struct.unpack('i', packed_id)[0]
-            # Read each numpy.float64
-            row_data = []
-            for _ in range(self.vec_size):
-                str_x = file.read(self.length).decode().strip()  # Remove the padding spaces
-                x = decimal.Decimal(str_x)
-                row_data.append(x)
-            row_data = np.array([float(d) for d in row_data])
-            return row_data
+            # Read the row
+            packed_data = file.read(self.int_size + self.vec_size * self.float_size)  # Size of one row (ID + vec_size * floats)
+            data = struct.unpack(f'i{self.vec_size}f', packed_data)
+            return np.array(data[1:])
 
 
 def test():
     # setup data
-    num_rows = 1000000
+    num_rows = 100000
     num_tests = 1000
     vec_size = 70
     # define instance of class
@@ -54,8 +51,11 @@ def test():
     bfh = BinaryFile(file_path)
     # create data and write to binary file
     records_np = np.random.random((num_rows, vec_size))
-    for i in range(num_rows):
-        bfh.insert_row(i,records_np[i])
+    # ##insert row by row
+    # for i in range(num_rows):
+    #     bfh.insert_row(i,records_np[i])
+    records_dict = [{"id": i, "embed": list(row)} for i, row in enumerate(records_np)]
+    bfh.insert_records(records_dict)
 
     count_test = 0
     failed_ids = []
@@ -64,7 +64,8 @@ def test():
         vec_ran = bfh.read_row(random_row_id)
         vec_real = records_np[random_row_id]
         # compare the 2 vectors if one is not equal break
-        if np.array_equal(vec_ran,vec_real):
+        # if np.array_equal(vec_ran,vec_real):
+        if np.allclose(vec_ran,vec_real):
             count_test += 1
         else:
             failed_ids.append(random_row_id)
@@ -74,11 +75,11 @@ def test():
     print(f"Failed ids: {failed_ids}")
 
 
-    # uncomment to print the vector
+    # ## uncomment to print the vector
     # print(records_np[1])
     # print('---------------------')
     # print(bfh.read_row(1))
-    # if np.array_equal(records_np[1],bfh.read_row(1)):
+    # if np.allclose(records_np[1],bfh.read_row(1)):
     #         print('-----------eq----------')
 
     # else:
