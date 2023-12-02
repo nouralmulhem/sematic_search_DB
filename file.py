51cc2ca250aa620371085bd47ea4195acf23ffa4
Ahmed Hany Farouk <67925988+Ahmed-H300@users.noreply.github.com>
2023-12-02 21:02:53 +0200

    Refactor read_row method and add read_all method

--- file.py
+++ file.py
@@ -18,16 +18,6 @@ class BinaryFile:
             # Write the packed data to the file
             file.write(packed_data)
 
-    def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
-        with open(self.filename, 'ab') as file:
-            for row in rows:
-                id, embed = row["id"], row["embed"]
-                # Pack the ID and the float values into a binary format
-                packed_data = struct.pack(f'i{self.vec_size}f', id, *embed)
-                # Write the packed data to the file
-                file.write(packed_data)
-                
-
     def read_row(self, row_id):
         with open(self.filename, 'rb') as file:
             # Calculate the position of the row
@@ -37,13 +27,36 @@ class BinaryFile:
             # Read the row
             packed_data = file.read(self.int_size + self.vec_size * self.float_size)  # Size of one row (ID + vec_size * floats)
             data = struct.unpack(f'i{self.vec_size}f', packed_data)
-            return np.array(data[1:])
+            return np.array(data)
+
+    def insert_records(self, rows: List[Dict[int, Annotated[List[float], 70]]]):
+        with open(self.filename, 'ab') as file:
+            for row in rows:
+                id, embed = row["id"], row["embed"]
+                # Pack the ID and the float values into a binary format
+                packed_data = struct.pack(f'i{self.vec_size}f', id, *embed)
+                # Write the packed data to the file
+                file.write(packed_data)
+
+    # read all rows
+    def read_all(self):
+        rows = []
+        with open(self.filename, 'rb') as file:
+            # iterate over all rows
+            while True:
+                # Read the row
+                packed_data = file.read(self.int_size + self.vec_size * self.float_size)
+                if packed_data == b'':
+                    break
+                data = struct.unpack(f'i{self.vec_size}f', packed_data)
+                rows.append(data)
+        return np.array(rows)
 
 
 def test():
     # setup data
     num_rows = 1000000
-    num_tests = 10000
+    num_tests = 100000
     vec_size = 70
     # define instance of class
     file_path = "data.bin"
@@ -66,7 +79,7 @@ def test():
     failed_ids = []
     for i in range(num_tests):
         random_row_id = random.randint(0, (num_tests - 1))
-        vec_ran = bfh.read_row(random_row_id)
+        vec_ran = bfh.read_row(random_row_id)[1:]
         vec_real = records_np[random_row_id]
         # compare the 2 vectors if one is not equal break
         # if np.array_equal(vec_ran,vec_real):
@@ -79,12 +92,21 @@ def test():
     print(f"Failed {len(failed_ids)} tests")
     print(f"Failed ids: {failed_ids}")
 
+    retrieved_all = bfh.read_all()
+    # remove id from retrieved all
+    retrieved_all = retrieved_all[:,1:]
+    # compare it with records_np
+    if np.allclose(retrieved_all,records_np):
+        print('all retrieved data are equal')
+    else:
+        print('all retrieved data are not equal')
+
 
     # ## uncomment to print the vector
     # print(records_np[1])
     # print('---------------------')
     # print(bfh.read_row(1))
-    # if np.allclose(records_np[1],bfh.read_row(1)):
+    # if np.allclose(records_np[1],bfh.read_row(1)[1:]):
     #         print('-----------eq----------')
 
     # else:
